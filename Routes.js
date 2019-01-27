78d534efab6330a0ea60bf79d28e7714b8a7f67e
sdong001 <sdong001@gmail.com>
2019-01-27 18:58:25 +0900

    nav styling

--- Routes.js
+++ Routes.js
@@ -10,13 +10,13 @@ import Discover from './Discover';
 
 const Routes = () => (
 		<Router>
-			<Scene key = "root">
-		  		<Scene key = "login" component = {Login} navTransparent initial = {true} />
+			<Scene key = "root" navBarButtonColor='#003300' navigationBarStyle={{backgroundColor : '#011a30'}} barButtonIconStyle={{tintColor: 'white'}}>
+		  		<Scene key = "login" component = {Login} navTransparent />
 		  		<Scene key = "discover" component = {Discover} navTransparent />
-				<Scene key = "home" component = {Home} navTransparent type = {ActionConst.RESET}/>
-			 	<Scene key = "pattern" component = {Pattern} title = "Light & Pattern"/>
-			 	<Scene key = "group" component = {Group} title = "Group" />
-			 	<Scene key = "accident" component = {Accident} title = "Accident" />
+				<Scene key = "home" component = {Home} navTransparent type = {ActionConst.RESET} initial = {true}/>
+			 	<Scene key = "pattern" component = {Pattern} title = "Light & Pattern" navBarButtonColor='#FFFFFF' titleStyle={{color: 'white'}} />
+			 	<Scene key = "group" component = {Group} title = "Group" navBarButtonColor='#FFFFFF' titleStyle={{color: 'white'}} />
+			 	<Scene key = "accident" component = {Accident} title = "Accident" navBarButtonColor='#FFFFFF' titleStyle={{color: 'white'}} />
 	      </Scene>
 	   </Router>
    );
