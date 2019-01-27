e908859328d1d40d51d6ec299984bc0cce842688
sdong001 <sdong001@gmail.com>
2019-01-27 19:45:08 +0900

    remove group page import

--- Routes.js
+++ Routes.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Router, Scene, ActionConst } from 'react-native-router-flux';
 import Home from './Home';
 import Pattern from './Pattern';
-import Group from './Group';
 import Accident from './Accident';
 import Login from './Login';
 import Discover from './Discover';
@@ -13,10 +12,10 @@ const Routes = () => (
 			<Scene key = "root" navBarButtonColor='#003300' navigationBarStyle={{backgroundColor : '#011a30'}} barButtonIconStyle={{tintColor: 'white'}}>
 		  		<Scene key = "login" component = {Login} navTransparent />
 		  		<Scene key = "discover" component = {Discover} navTransparent />
-				<Scene key = "home" component = {Home} navTransparent type = {ActionConst.RESET} initial = {true}/>
+				<Scene key = "home" component = {Home} navTransparent type = {ActionConst.RESET} />
 			 	<Scene key = "pattern" component = {Pattern} title = "Light & Pattern"
 					navBarButtonColor='#FFFFFF' titleStyle={{color: 'white'}} />
-			 	<Scene key = "accident" component = {Accident} title = "Accident"
+			 	<Scene key = "accident" component = {Accident} title = "Accident" initial = {true}
 					navBarButtonColor='#FFFFFF' titleStyle={{color: 'white'}} />
 	      </Scene>
 	   </Router>
