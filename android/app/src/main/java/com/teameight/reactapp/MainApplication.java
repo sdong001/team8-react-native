74540a684eb9427ef30b0c3ba0949d4c0c34108d
팀에잇 <tim-eis@tim-eis-ui-iMac-2.local>
2019-01-31 21:28:32 +0900

    Modify: rename App

--- android/app/src/main/java/com/teameight/reactapp/MainApplication.java
+++ android/app/src/main/java/com/teameight/reactapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.teameight.reactapp;
 
 import android.app.Application;
 
--- ios/EIGHTTests/EIGHTTests.m
+++ ios/EIGHTTests/EIGHTTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface EIGHTTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation EIGHTTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
